f83a6cf56c5aa3e80caf0c317c051132cf66385a
Yoong Jian <iuncleblade_yj@hotmail.com>
2018-03-15 23:47:06 +0800

    querying and writing

--- playground/mongodb_connect.js
+++ playground/mongodb_connect.js
@@ -1,22 +1,22 @@
-const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/RestaurantAPI', (err,client)=>{
   if(err) {
     return console.log('Unable to connect to MongDB',err);
   }
   console.log('Connected to MongoDB');
-  const db = client.db('Bookings');
+  // const db = client.db('Bookings');
 
-  db.collection('Bookings').insertOne({
-    name:'John',
-    contact_number: '0102438625',
-    seats: '2',
-    notes: 'just a random note'
-  }, (err,result)=>{
-    if(err) {
-      return console.log('Cannot write to db',err);
-    }
-    console.log(JSON.stringify(result.ops, undefined, 2));
-  })
+  // db.collection('Bookings').insertOne({
+  //   name:'John',
+  //   contact_number: '0102438625',
+  //   seats: '2',
+  //   notes: 'just a random note'
+  // }, (err,result)=>{
+  //   if(err) {
+  //     return console.log('Cannot write to db',err);
+  //   }
+  //   console.log(JSON.stringify(result.ops,undefined,2));
+  // })
   client.close();
 })

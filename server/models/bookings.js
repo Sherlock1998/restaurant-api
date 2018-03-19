7d59b858296af3c93f129d910124fbb516d128b1
Yoong Jian <iuncleblade_yj@hotmail.com>
2018-03-20 00:02:02 +0800

    added testing for POST/bookings

--- server/models/bookings.js
+++ server/models/bookings.js
@@ -5,26 +5,26 @@ var bookings = mongoose.model('bookings', {
     type:String,
     required:true,
   },
-  // contactNumber: {
-  //   type:Number,
-  //   trim:true
-  // },
-  // seats: {
-  //   type:Number,
-  //
-  //   min:1
-  // },
-  // notes: {
-  //   type:String,
-  // },
-  // confirmed: {
-  //   type:Boolean,
-  //   default:false,
-  // },
-  // confirmedAt: {
-  //   type:Number,
-  //   default:null,
-  // }
+  contactNumber: {
+    type:Number,
+    trim:true
+  },
+  seats: {
+    type:Number,
+
+    min:1
+  },
+  notes: {
+    type:String,
+  },
+  confirmed: {
+    type:Boolean,
+    default:false,
+  },
+  confirmedAt: {
+    type:Number,
+    default:null,
+  }
 });
 
 module.exports = {bookings}
--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
-var express = require('express');
-var bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser')
 
-var {mongoose} = require('./db/mongoose');
-var {bookings} = require('./models/bookings');
+const {mongoose} = require('./db/mongoose');
+const {bookings} = require('./models/bookings');
 
-var app = express();
+const app = express();
 
 //bodyParser allows express to post bodies
 app.use(bodyParser.json());
 
 app.post('/bookings',(req,res)=>{
   // console.log(req.body);
-  var booking = new bookings({
-    name: req.body.name
+  const booking = new bookings({
+    name: req.body.name,
+    contactNumber: req.body.contactNumber
   });
 
   booking.save().then((doc)=>{
@@ -25,3 +26,5 @@ app.post('/bookings',(req,res)=>{
 app.listen(3000,()=>{
   console.log('Server started on port 3000');
 });
+
+module.exports = {app};
